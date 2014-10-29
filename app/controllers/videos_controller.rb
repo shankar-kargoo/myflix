3220541bc1bf4799012dcd0c7963b331c4d20314
Shankar Narayan <shankar@kargoo.com>
2014-10-28 23:29:58 -0700

    Display Video/index and added categories with M:M relation)

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,11 +2,11 @@ class VideosController < ApplicationController
   
   def index
     @videos = Video.all
+    @categories = Category.all
   end
 
   def show
     @video = Video.find_by(:id => params[:id])
-
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Myflix::Application.routes.draw do
   
   get 'ui(/:action)', controller: 'ui'
   resources :videos
+  resources :categories
 end
